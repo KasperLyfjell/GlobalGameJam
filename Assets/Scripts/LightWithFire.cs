9cc3a5cbf1d45869305b6ffd456785192691bc2f
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2023-02-04 11:17:43 +0100

    Torch & Fireplace

---
+++ Assets/Scripts/LightWithFire.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightWithFire : MonoBehaviour
+{
+    public GameObject fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fireball")
+        {
+            fire.SetActive(true);
+        }
+    }
+}
