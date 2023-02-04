cff9bf9398b56a1bb40dcc252b744abf722e67de
Kasper Lyfjell <kasper.lyfjell@hotmail.no>
2023-02-04 12:26:06 +0100

    Started Flower Script

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    public GameObject fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fireball")
+        {
+            fire.SetActive(true);
+        }
+
+        if (other.tag == "Water")
+        {
+            fire.SetActive(false);
+        }
+    }
+}
